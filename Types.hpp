a4f6bd449875fc8707bec972a30b77232e13589f
Paul Scharnofske <asynts@gmail.com>
2022-10-25 11:24:46 +0200

    Implement serialization of Transactions
    We can store/load transactions from/to disk.

--- Types.hpp
+++ Types.hpp
@@ -5,6 +5,7 @@
 #include <QRandomGenerator64>
 #include <QMetaObject>
 #include <QDebug>
+#include <QDateTime>
 
 namespace Accounting
 {
@@ -25,9 +26,8 @@ namespace Accounting
         return result;
     }
 
-    class Id {
-    public:
-        const QString m_value;
+    struct Id {
+        QString m_value;
 
         static Id create_random();
     };
@@ -40,10 +40,111 @@ namespace Accounting
         return Id{ to_base_58(value) };
     }
 
-    inline QDebug operator<<(QDebug debug, const Id &value) {
+    inline QDebug operator<<(QDebug debug, const Id& value) {
         debug.nospace() << value.m_value;
         return debug;
+    }
+
+    inline QDataStream& operator<<(QDataStream& out, const Id& value) {
+        out << value.m_value;
+        return out;
+    }
+
+    inline QDataStream& operator>>(QDataStream& in, Id& value) {
+        in >> value.m_value;
+        return in;
+    }
+
+    struct Transaction {
+        Id m_id;
+        std::optional<Id> m_previous_version_id;
+        QDateTime m_timestamp;
+
+        QString m_category;
+        std::optional<QString> m_description;
+        QDate m_date;
+        qreal m_amount;
+    };
+
+    inline QDebug operator<<(QDebug debug, const Transaction& value) {
+        debug.nospace() << "Transaction("
+                        << "id=" << value.m_id << ", ";
+
+        if (value.m_previous_version_id.has_value()) {
+            debug.nospace() << "previous_version_id=" << value.m_previous_version_id.value() << ", ";
+        } else {
+            debug.nospace() << "previous_version_id=(null), ";
+        }
+
+        debug.nospace() << "date=" << value.m_date << ", ";
+        debug.nospace() << "category=" << value.m_category << ", ";
+        debug.nospace() << "amount=" << value.m_amount << ", ";
+
+        if (value.m_description.has_value()) {
+            debug.nospace() << "description=" << value.m_description.value() << ", ";
+        } else {
+            debug.nospace() << "description=(null), ";
+        }
+
+        debug.nospace() << "timestamp=" << value.m_timestamp << ", ";
+
+        return debug;
+    }
+
+    inline QDataStream& operator<<(QDataStream& out, const Transaction& value) {
+        out << value.m_id;
+
+        if (value.m_previous_version_id.has_value()) {
+            out << true;
+            out << value.m_previous_version_id.value();
+        } else {
+            out << false;
+        }
+
+        out << value.m_timestamp;
+        out << value.m_category;
+
+        if (value.m_description.has_value()) {
+            out << true;
+            out << value.m_description.value();
+        } else {
+            out << false;
+        }
+
+        out << value.m_date;
+        out << value.m_amount;
+
+        return out;
+    }
+
+    inline QDataStream& operator>>(QDataStream& in, Transaction& value) {
+        in >> value.m_id;
+
+        bool has_previous_version_id;
+        in >> has_previous_version_id;
+        if (has_previous_version_id) {
+            in >> value.m_previous_version_id.value();
+        } else {
+            value.m_previous_version_id = std::nullopt;
+        }
+
+        in >> value.m_timestamp;
+        in >> value.m_category;
+
+        bool has_description;
+        in >> has_description;
+        if (has_description) {
+            in >> value.m_description.value();
+        } else {
+            value.m_description = std::nullopt;
+        }
+
+        in >> value.m_date;
+        in >> value.m_amount;
+
+        return in;
     }
 }
 
 Q_DECLARE_METATYPE(Accounting::Id);
+Q_DECLARE_METATYPE(Accounting::Transaction);
--- main.cpp
+++ main.cpp
@@ -2,17 +2,41 @@
 #include "Types.hpp"
 
 #include <QApplication>
+#include <QBuffer>
 
 int main(int argc, char *argv[])
 {
-    qDebug() << "result: " << Accounting::Id::create_random();
-    qDebug() << "result: " << Accounting::Id::create_random();
-    qDebug() << "result: " << Accounting::Id::create_random();
-    qDebug() << "result: " << Accounting::Id::create_random();
+    // Create a transaction in memory.
+    Accounting::Transaction transaction_1{
+        .m_id = Accounting::Id::create_random(),
+        .m_previous_version_id = std::nullopt,
+        .m_timestamp = QDateTime::currentDateTime(),
+        .m_category = "Groceries",
+        .m_description = std::nullopt,
+        .m_date = QDate(2022, 10, 24),
+        .m_amount = 42.00,
+    };
+
+    qDebug() << transaction_1;
+
+    QByteArray buffer;
+    QDataStream stream{ &buffer, QIODevice::ReadWrite };
+
+    // Serialize it into the buffer.
+    stream << transaction_1;
+
+    Accounting::Transaction transaction_2;
+
+    // Deserialize it from the buffer.
+    stream.device()->reset();
+    stream >> transaction_2;
+
+    qDebug() << transaction_2;
 
     QApplication application(argc, argv);
 
     qRegisterMetaType<Accounting::Id>();
+    qRegisterMetaType<Accounting::Transaction>();
 
     MainWindow main_window;
     main_window.show();
