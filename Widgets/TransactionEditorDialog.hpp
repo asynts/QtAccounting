a81ca10d256b7229110ce755718cd586d000ea93
Paul Scharnofske <asynts@gmail.com>
2022-11-01 22:02:05 +0100

    Make 'Ui::TransactionEditorDialog' a member instead
    This removes some redundancy.

--- Widgets/TransactionEditorDialog.hpp
+++ Widgets/TransactionEditorDialog.hpp
@@ -16,8 +16,7 @@ namespace Accounting::Widgets
         TransactionEditorDialog(Persistance::TransactionObject *transaction_object, QWidget *parent = nullptr)
             : QDialog(parent)
         {
-            Ui::TransactionEditorDialog ui;
-            ui.setupUi(this);
+            m_ui.setupUi(this);
 
             if (transaction_object == nullptr) {
                 setWindowTitle("New Transaction");
@@ -25,43 +24,37 @@ namespace Accounting::Widgets
                 setWindowTitle(QString("Edit Transaction '%1'").arg(transaction_object->id()));
             }
 
-            m_category_QComboBox = ui.m_category_QComboBox;
-            m_type_QComboBox = ui.m_type_QComboBox;
-            m_amount_QLineEdit = ui.m_amount_QLineEdit;
-            m_date_QDateEdit = ui.m_date_QDateEdit;
-            m_buttons_QDialogButtonBox = ui.m_buttons_QDialogButtonBox;
-
             {
-                m_category_QComboBox->addItem("Groceries");
-                m_category_QComboBox->setCurrentIndex(0);
+                m_ui.m_category_QComboBox->addItem("Groceries");
+                m_ui.m_category_QComboBox->setCurrentIndex(0);
 
-                connect(m_category_QComboBox, &QComboBox::currentTextChanged,
+                connect(m_ui.m_category_QComboBox, &QComboBox::currentTextChanged,
                         this, &TransactionEditorDialog::slotValidate);
             }
 
             {
-                m_type_QComboBox->addItem("Expense");
-                m_type_QComboBox->addItem("Income");
-                m_type_QComboBox->setCurrentIndex(0);
+                m_ui.m_type_QComboBox->addItem("Expense");
+                m_ui.m_type_QComboBox->addItem("Income");
+                m_ui.m_type_QComboBox->setCurrentIndex(0);
 
-                connect(m_type_QComboBox, &QComboBox::currentIndexChanged,
+                connect(m_ui.m_type_QComboBox, &QComboBox::currentIndexChanged,
                         this, &TransactionEditorDialog::slotValidate);
             }
 
             {
                 auto *validator = new QDoubleValidator(this);
                 validator->setBottom(0.00);
-                m_amount_QLineEdit->setValidator(validator);
-                m_amount_QLineEdit->setText("0.00");
+                m_ui.m_amount_QLineEdit->setValidator(validator);
+                m_ui.m_amount_QLineEdit->setText("0.00");
 
-                connect(m_amount_QLineEdit, &QLineEdit::textChanged,
+                connect(m_ui.m_amount_QLineEdit, &QLineEdit::textChanged,
                         this, &TransactionEditorDialog::slotValidate);
             }
 
             {
-                m_date_QDateEdit->setDate(QDate::currentDate());
+                m_ui.m_date_QDateEdit->setDate(QDate::currentDate());
 
-                connect(m_date_QDateEdit, &QDateEdit::dateChanged,
+                connect(m_ui.m_date_QDateEdit, &QDateEdit::dateChanged,
                         this, &TransactionEditorDialog::slotValidate);
             }
 
@@ -79,17 +72,17 @@ namespace Accounting::Widgets
         void slotLoad(Persistance::TransactionData transaction_data) {
             m_old_transaction_data = transaction_data;
 
-            m_category_QComboBox->setCurrentText(transaction_data.m_category);
+            m_ui.m_category_QComboBox->setCurrentText(transaction_data.m_category);
 
             if (transaction_data.m_amount <= 0.00) {
-                m_type_QComboBox->setCurrentIndex(0); // Expense
+                m_ui.m_type_QComboBox->setCurrentIndex(0); // Expense
             } else {
-                m_type_QComboBox->setCurrentIndex(1); // Income
+                m_ui.m_type_QComboBox->setCurrentIndex(1); // Income
             }
 
-            m_amount_QLineEdit->setText(QString::number(std::abs(transaction_data.m_amount), 'f', 2));
+            m_ui.m_amount_QLineEdit->setText(QString::number(std::abs(transaction_data.m_amount), 'f', 2));
 
-            m_date_QDateEdit->setDate(transaction_data.m_date);
+            m_ui.m_date_QDateEdit->setDate(transaction_data.m_date);
 
             slotValidate();
         }
@@ -97,19 +90,19 @@ namespace Accounting::Widgets
         std::optional<Persistance::TransactionData> slotValidate() {
             bool is_valid = true;
 
-            if (m_category_QComboBox->currentText().trimmed().isEmpty()) {
+            if (m_ui.m_category_QComboBox->currentText().trimmed().isEmpty()) {
                 is_valid = false;
             }
 
-            if (!m_amount_QLineEdit->hasAcceptableInput()) {
+            if (!m_ui.m_amount_QLineEdit->hasAcceptableInput()) {
                 is_valid = false;
             }
 
-            if (!m_date_QDateEdit->hasAcceptableInput()) {
+            if (!m_ui.m_date_QDateEdit->hasAcceptableInput()) {
                 is_valid = false;
             }
 
-            m_buttons_QDialogButtonBox->button(QDialogButtonBox::Ok)->setEnabled(is_valid);
+            m_ui.m_buttons_QDialogButtonBox->button(QDialogButtonBox::Ok)->setEnabled(is_valid);
 
             if (is_valid) {
                 QString id;
@@ -122,9 +115,9 @@ namespace Accounting::Widgets
                 return Persistance::TransactionData{
                     .m_id = id,
                     .m_timestamp_created = QDateTime::currentDateTimeUtc(),
-                    .m_date = m_date_QDateEdit->date(),
-                    .m_amount = m_amount_QLineEdit->text().toDouble(),
-                    .m_category = m_category_QComboBox->currentText().trimmed(),
+                    .m_date = m_ui.m_date_QDateEdit->date(),
+                    .m_amount = m_ui.m_amount_QLineEdit->text().toDouble(),
+                    .m_category = m_ui.m_category_QComboBox->currentText().trimmed(),
                 };
             } else {
                 return std::nullopt;
@@ -139,12 +132,7 @@ namespace Accounting::Widgets
         }
 
     private:
+        Ui::TransactionEditorDialog m_ui;
         std::optional<Persistance::TransactionData> m_old_transaction_data;
-
-        QComboBox *m_category_QComboBox;
-        QComboBox *m_type_QComboBox;
-        QLineEdit *m_amount_QLineEdit;
-        QDateEdit *m_date_QDateEdit;
-        QDialogButtonBox *m_buttons_QDialogButtonBox;
     };
 }
