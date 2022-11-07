14892b2621c1ab58c933a05b4c986fb056668b9a
Paul Scharnofske <asynts@gmail.com>
2022-11-07 10:58:28 +0100

    Work: Rewriting the S3 logic
    This is already looking much better, but I still haven't decided, if I
    should store files locally.

--- Persistance/S3.hpp
+++ Persistance/S3.hpp
@@ -1,18 +1,33 @@
 #pragma once
 
 #include <fstream>
+#include <filesystem>
+
+#include <fmt/format.h>
 
 #include <QString>
 #include <QSettings>
 
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include <aws/s3/model/GetObjectRequest.h>
 #include <aws/core/auth/AWSCredentials.h>
+
+#include "Persistance/Database.hpp"
+
+// FIXME: Find a better name for this file.
 
 namespace Accounting::Persistance
 {
-    // FIXME: Reuse.
-    inline Aws::S3::S3Client s3_client() {
+    constexpr const char *ALLOCATION_TAG = "AccountingTag";
+
+    inline Aws::S3::S3Client get_s3_client() {
+        static std::optional<Aws::S3::S3Client> s3_client;
+
+        if (s3_client.has_value()) {
+            return s3_client.value();
+        }
+
         QSettings settings;
 
         Aws::Auth::AWSCredentials credentials;
@@ -22,11 +37,11 @@ namespace Accounting::Persistance
         Aws::Client::ClientConfiguration clientConfiguration;
         clientConfiguration.region = settings.value("AWS/Region").value<QString>().toStdString();
 
-        Aws::S3::S3Client client(credentials, clientConfiguration);
-
-        return client;
+        s3_client.emplace(credentials, clientConfiguration);
+        return s3_client.value();
     }
 
+    /*
     inline void upload_file(QString localPath, QString remotePath) {
         std::string remotePath_std = remotePath.toStdString();
         std::string localPath_std = localPath.toStdString();
@@ -45,6 +60,47 @@ namespace Accounting::Persistance
         // FIXME: Do this asynchronous
         // FIXME: Deal with errors
         auto outcome = client.PutObject(request);
+        Q_ASSERT(outcome.IsSuccess());
+    }
+    */
+
+    inline void update_symbolic_link(std::filesystem::path path) {
+        // FIXME
+    }
+
+    inline std::filesystem::path fetch_latest_from_s3() {
+        std::filesystem::path localPath{ QStandardPaths::writableLocation(QStandardPaths::StandardLocation::AppDataLocation).toStdString() };
+        localPath /= "Database";
+        localPath /= fmt::format("{:016x}_Database.bin", QDateTime::currentMSecsSinceEpoch());
+
+        Aws::S3::Model::GetObjectRequest request;
+        request.SetBucket("accounting-23fbf6ce-ff25-4f6a-a75a-b50bf814fc62");
+        request.SetKey("/Database/Database.bin");
+        request.SetResponseStreamFactory([localPath] {
+            return Aws::New<Aws::FStream>(ALLOCATION_TAG, localPath, std::ios_base::out | std::ios_base::binary);
+        });
+
+        // FIXME: Async.
+        // FIXME: Error handling.
+        auto outcome = get_s3_client().GetObject(request);
+        Q_ASSERT(outcome.IsSuccess());
+
+        return localPath;
+    }
+
+    inline void upload_latest_to_s3(std::filesystem::path localPath) {
+        auto inputStream = Aws::MakeShared<Aws::FStream>(ALLOCATION_TAG, localPath, std::ios_base::in | std::ios_base::binary);
+
+        std::string remotePath = fmt::format("/Database/{}", localPath.filename().string());
+
+        Aws::S3::Model::PutObjectRequest request;
+        request.SetBucket("accounting-23fbf6ce-ff25-4f6a-a75a-b50bf814fc62");
+        request.SetKey(remotePath);
+        request.SetBody(inputStream);
+
+        // FIXME: Async.
+        // FIXME: Error handling.
+        auto outcome = get_s3_client().PutObject(request);
         Q_ASSERT(outcome.IsSuccess());
     }
 }
